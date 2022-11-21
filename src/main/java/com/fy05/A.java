9a068329ec05910ffd60391d12a10146f6bb46f5
fy05 <fy05@qq.com>
2022-11-21 20:49:41 +0800

    first

---
+++ src/main/java/com/fy05/A.java
@@ -0,0 +1,12 @@
+package com.fy05;
+
+public class A {
+    int a=10;
+    {
+        a=30;
+    }
+    A(){
+        a=20;
+    }
+
+}
---
+++ src/main/java/com/fy05/ClassLoaderTest.java
@@ -0,0 +1,12 @@
+package com.fy05;
+
+
+import java.util.ArrayDeque;
+import java.util.Stack;
+
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+
+    }
+}
+
